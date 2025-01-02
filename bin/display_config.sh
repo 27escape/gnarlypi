64021c1059dac26f0b46060d303ba6511ca40ba5
Kevin Mulholland <kevin.mulholland@bbc.co.uk>
2025-01-02 11:24:50 +0000

    fix(display_status): clean up directory use in the script

--- bin/display_config.sh
+++ bin/display_config.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # choose which displays to use and create the start_displays.sh script
 
-FULL_PATH="$HOME/gnarlypi/bin"
+FULL_PATH="$HOME/gnarlypi"
 cd "$FULL_PATH" || (echo "cannot find dir $FULL_PATH" && exit 1)
 
-DISPLAY_SCRIPT=".$FULL_PATH/start_displays.sh"
+DISPLAY_SCRIPT="$FULL_PATH/bin/start_displays.sh"
+STATUS_DIR="$FULL_PATH/status"
 
 # ----------------------------------------------------------------------------
 
@@ -29,14 +30,14 @@ if [ ! -f "$DISPLAY_SCRIPT" ] || prompt_yn "Do you wish to update the display ch
 # reconfigure with the display_config.sh script,
 # \$GNARLY_LOG comes from be_gnarly script
 # do not modify by hand
-cd '$FULL_PATH'
+cd '$STATUS_DIR'
 " > "$DISPLAY_SCRIPT"
   chmod a+x "$DISPLAY_SCRIPT"
 
   # "curses" and "basic" basic would need to be launched as an alternative
   # login program to display on the console, rather than the usual bash login
 
-  for display in ../status/gnarly_status_*  ; do
+  for display in status/gnarly_status_*  ; do
     display_name=$(basename "$display")
     if prompt_yn "Do you wish to install $display_name" ; then
       echo "nohup \"./$display_name\" >> \$GNARLY_LOG 2>&1 &" >> "$DISPLAY_SCRIPT"
