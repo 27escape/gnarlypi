fe46f6bee9423aa1a5d925bd962f45cc46ee2dcc
Kevin Mulholland <kevin.mulholland@bbc.co.uk>
2025-09-01 10:22:37 +0100

    feat: consolidate logging

--- libs/debug.py
+++ libs/debug.py
@@ -1,13 +1,53 @@
 # consistent logging setup for the applications
 
+import os
 import logging
+import inspect
 from logging.handlers import TimedRotatingFileHandler
+
+
+class DebugLogger:
+    def __init__(self, logger=None):
+        self.logger = logger or logging.getLogger(__name__)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s %(levelname)s %(caller_file)s:%(caller_line)d %(message)s'
+        )
+        handler.setFormatter(formatter)
+        if not self.logger.handlers:
+            self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
+
+    def _log(self, level, msg, *args, **kwargs):
+        # Get caller's frame
+        frame = inspect.currentframe().f_back.f_back
+        filename = os.path.basename(frame.f_code.co_filename)
+        lineno = frame.f_lineno
+        extra = {'caller_file': filename, 'caller_line': lineno}
+        self.logger.log(level, msg, *args, extra=extra, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        self._log(logging.INFO, msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        self._log(logging.ERROR, msg, *args, **kwargs)
+
+    def warn(self, msg, *args, **kwargs):
+        self._log(logging.WARNING, msg, *args, **kwargs)
+
+    def debug(self, msg, *args, **kwargs):
+        self._log(logging.DEBUG, msg, *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        self._log(logging.CRITICAL, msg, *args, **kwargs)
+
 
 
 def Debug(name, logfile=None, level=logging.INFO):
     """Initialize logging for the application."""
 
     logger = logging.getLogger(name=__file__)      
+    # debugger = DebugLogger( logger)     
       
     match level.lower():
         case "debug" | logging.DEBUG:
@@ -39,7 +79,7 @@ def Debug(name, logfile=None, level=logging.INFO):
         }
               
         logging.basicConfig( **logging_params)
-        logger.info("Started")
+        # debugger.info("Started")
     else:
         if loglevel != logging.NOTSET:
             logging.basicConfig(
