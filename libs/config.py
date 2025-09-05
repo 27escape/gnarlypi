ac75dcc4138b94435b3f2284113f111dbf82c290
Kevin Mulholland <kevin.mulholland@bbc.co.uk>
2025-09-05 12:18:26 +0100

    fix: resolve sporadic issue with env variables not being added to config data

--- libs/config.py
+++ libs/config.py
@@ -31,15 +31,25 @@ class Config:
         self.updated = False
         self._load()
 
+    # def _substitute_env_vars(self, content):
+    #     """Substitute environment variables in the YAML content."""
+    #     # Environment variable pattern ${HOME}
+    #     env_pattern = re.compile(r"\$\{([^}^{]+)\}")
+    #     # Substitute environment variables
+    #     content = env_pattern.sub(
+    #         lambda match: os.getenv(match.group(1), match.group(0)), content
+    #     )
+    #     return content
+
     def _substitute_env_vars(self, content):
         """Substitute environment variables in the YAML content."""
-        # Environment variable pattern ${HOME}
         env_pattern = re.compile(r"\$\{([^}^{]+)\}")
         # Substitute environment variables
-        content = env_pattern.sub(
-            lambda match: os.getenv(match.group(1), match.group(0)), content
-        )
-        return content
+        def env_replace(match):
+            var = match.group(1)
+            value = os.getenv(var)
+            return value if value is not None else match.group(0)
+        return env_pattern.sub(env_replace, content)
 
     def _substitute_references(self):
         """Substitute references to other fields in the loaded YAML data."""
